581c2b9c8cf0a571dfd981012bf89634cddc2704
emonroy58 <emgmonroy@gmail.com>
2019-03-08 04:38:57 -0600

    Elemtos CSS

--- js/login.js
+++ js/login.js
@@ -110,7 +110,7 @@ function observer() {
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       console.log('hay usuario')
-      document.getElementById('user-name').innerHTML= user.displayName;
+      document.getElementById('user-name').innerHTML= `${user.displayName}`;
       document.getElementById('cliente-photo').setAttribute("src",user.photoURL);
       // User is signed in.
       var displayName = user.displayName;
@@ -171,6 +171,10 @@ const obtainUser = () => {
 }
 
 
+const btnGoogle = document.getElementById('btn-google');
+  btnGoogle.addEventListener('click', ()=>{
+    googleSigIn()
+  }  );
 
 function googleSigIn(){
 
@@ -197,10 +201,7 @@ if(location.href.includes('login.html')){
     singIn();
   });
 
-  const btnGoogle = document.getElementById('btn-google');
-  btnGoogle.addEventListener('click', ()=>{
-    googleSigIn()
-  }  );
+  
 }
 else if(location.href.includes('editprofile.html')){
   const signOutButton = document.getElementById('signOut-button');
