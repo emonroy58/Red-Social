8e549e905225a0854dbc6e3134bf990085406def
emonroy58 <emgmonroy@gmail.com>
2019-03-11 12:10:57 -0600

    ÃInicializa la BD

--- js/profile.js
+++ js/profile.js
@@ -60,3 +60,18 @@ editButton.addEventListener('click', function () {
 
  const signOutButton = document.getElementById('signOut-button');
  signOutButton.addEventListener('click', singOut)
+
+
+ /*Uso de la BD*/
+
+ db.collection("users").add({
+  first: "Ada",
+  last: "Lovelace",
+  born: 1815
+})
+.then(function(docRef) {
+  console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+  console.error("Error adding document: ", error);
+});
