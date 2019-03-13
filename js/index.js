a207217cf90eef98de442977fbb8bdcc37e12987
DianaLiz11 <dianamunoz86@live.com.mx>
2019-03-13 13:26:56 -0600

    Se agregan archivos de libreria y router para convertir proyecto en SPA

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
-const signupButton = document.getElementById('signup-button');
-const loginButton = document.getElementById('login-button');
-
-signupButton.addEventListener('click', ()=>{
-  window.location = './login.html';
-  localStorage.option = 'signup';
-});
-
-
-loginButton.addEventListener('click', ()=>{
-  window.location = './login.html';
-  localStorage.option = 'login';
-});
+// const signupButton = document.getElementById('signup-button');
+// const loginButton = document.getElementById('login-button');
+//
+// signupButton.addEventListener('click', ()=>{
+//   window.location = './login.html';
+//   localStorage.option = 'signup';
+// });
+//
+//
+// loginButton.addEventListener('click', ()=>{
+//   window.location = './login.html';
+//   localStorage.option = 'login';
+// });
