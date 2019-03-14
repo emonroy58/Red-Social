03b2f51f6596c56dd40dab9b2eb57e48debf3697
DianaLiz11 <dianamunoz86@live.com.mx>
2019-03-14 16:00:40 -0600

    se agrega funciones de borrar y editar post

--- src/js/controllers/editProfile.js
+++ src/js/controllers/editProfile.js
@@ -1,5 +1,40 @@
 let isEditable = false;
 
+function deletePost(userId,docId) {
+  db.collection("posts").doc(userId).collection('private_post').doc(docId).delete()
+  .then(function() {
+    console.log("Document successfully deleted!");
+  }).catch(function(error) {
+    console.error("Error removing document: ", error);
+  });
+}
+
+function printData()  {
+  var tabla = document.getElementById('tabla');
+  const user = firebase.auth().currentUser;
+  db.collection('posts').doc(user.uid).collection('private_post').onSnapshot((querySnapshot) => {
+    tabla.innerHTML = '';
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id}=>${doc.data()}`);
+      tabla.innerHTML+=`
+      <tr>
+        <td>
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">${doc.data().userName}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">${doc.data().time}</h6>
+              <p class="card-text">${doc.data().message}</p>
+              <button class="btn btn-primary" type="submit"onclick="updatePost('${user.uid}','${doc.id}','${doc.data().message}')"><i class="far fa-edit"></i></button>
+              <button class="btn btn-primary" type="submit" onclick="deletePost('${user.uid}','${doc.id}')"><i class="far fa-trash-alt"></i></button>
+            </div>
+          </div>
+        </td>
+      </tr>
+      `
+    })
+  })
+}
+
 (function (window, document) {
   library.controller('editprofile', {
     observer: function () {
@@ -7,7 +42,7 @@ let isEditable = false;
        firebase.auth().onAuthStateChanged(function(user) {
          if (user) {
            console.log('hay usuario')
-
+           printData();
            var displayName = user.displayName;
            if(displayName == null){
              displayName = user.email;
@@ -47,6 +82,30 @@ let isEditable = false;
          editButton.innerHTML = 'Edit';
        }
      },
+     addPost: function() {
+       let d = new Date(); //obtener fecha
+       let fechaHoy =  d.getDate()+"/"+(d.getMonth()+1)+"/"+d.getFullYear();
+       const postField = document.getElementById('post-field');
+       const user = firebase.auth().currentUser;
+       if (postField.value != null) {
+         db.collection("posts").doc(user.uid).collection('private_post').add({
+             userName: user.displayName,
+             message: postField.value,
+             time: fechaHoy,
+             isPublic: false,
+             likes: [],
+             comments: []
+           })
+
+           .then(function(docRef) {
+             console.log("Document written with ID: ", docRef.id);
+
+           })
+           .catch(function(error) {
+             console.error("Error adding document: ", error);
+           });
+       }
+     },
      signOut: function () {
        firebase.auth().signOut()
        .then(function() {
@@ -60,3 +119,30 @@ let isEditable = false;
      }
   });
 })(window, document)
+
+
+function updatePost (userId,docId,message){
+  document.getElementById('post-field').value = message;
+  const button = document.getElementById('add-btn');
+
+  button.innerHTML = 'Editar';
+
+  button.onclick = function(){
+    var postRef = db.collection("posts").doc(userId).collection('private_post').doc(docId);
+    const postField = document.getElementById('post-field').value;
+    let d = new Date(); //obtener fecha
+    let fechaHoy =  d.getDate()+"/"+(d.getMonth()+1)+"/"+d.getFullYear();
+    return postRef.update({
+      message: postField,
+      time: fechaHoy
+    })
+    .then(function() {
+      console.log("Document successfully updated!");
+    })
+    .catch(function(error) {
+      // The document probably doesn't exist.
+      console.error("Error updating document: ", error);
+    });
+  }
+
+}
